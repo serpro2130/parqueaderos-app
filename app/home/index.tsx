020713fe5da68b67c0790cbf9a824b127a6a02da
Hector Alarcon Guerra <serproemcamseguridadprivada@gmail.com>
2024-12-20 11:40:06 -0500

    componente boton

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,12 +1,24 @@
 import CustomButton from '@/components/shared/CustomButton';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
 import { View, Text, SafeAreaView } from 'react-native'
 
 const HomeScreen = () => {
     return (
         <SafeAreaView>
             <View className='px-10 mt-5'>
-                <CustomButton />
+                <Link href='/products' asChild>
+                    <CustomButton className="mb-10" color='primary'>Productos </CustomButton>
+                </Link>
+                <CustomButton
+                    className="mb-10"
+                    color='primary'
+                    onPress={() => router.push('/products')}
+                >Productos </CustomButton>
+                <CustomButton
+                    variant='text-only'
+                    onPress={() => router.push('/products')}
+                    className="mb-10"
+                >Productos </CustomButton>
                 {/* <Link className='mb-5' href='/products'>Productos</Link>
                 <Link className='mb-5' href='/profile'>Perfil</Link>
                 <Link className='mb-5' href='/settings'>Ajustes</Link> */}
