651021778a9955cf7cdb7df08beba8b9c0f9982f
Hector Alarcon Guerra <serproemcamseguridadprivada@gmail.com>
2024-12-20 11:51:06 -0500

    stack

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -6,19 +6,34 @@ const HomeScreen = () => {
     return (
         <SafeAreaView>
             <View className='px-10 mt-5'>
-                <Link href='/products' asChild>
-                    <CustomButton className="mb-10" color='primary'>Productos </CustomButton>
-                </Link>
+
                 <CustomButton
-                    className="mb-10"
+                    className="mb-2"
                     color='primary'
                     onPress={() => router.push('/products')}
-                >Productos </CustomButton>
+                >
+                    Productos
+                </CustomButton>
+
                 <CustomButton
-                    variant='text-only'
-                    onPress={() => router.push('/products')}
-                    className="mb-10"
-                >Productos </CustomButton>
+                    onPress={() => router.push('/profile')}
+                    className="mb-2"
+                    color="secondary"
+                >
+                    Profile
+                </CustomButton>
+
+                <CustomButton
+                    onPress={() => router.push('/settings')}
+                    className="mb-2"
+                    color="tertiary"
+                >
+                    Ajustes
+                </CustomButton>
+
+                <Link href='/products' asChild>
+                    <CustomButton variant='text-only' className="mb-10" color='primary'>Productos </CustomButton>
+                </Link>
                 {/* <Link className='mb-5' href='/products'>Productos</Link>
                 <Link className='mb-5' href='/profile'>Perfil</Link>
                 <Link className='mb-5' href='/settings'>Ajustes</Link> */}
