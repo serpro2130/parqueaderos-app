3571f1b8f08c9dcaf503fcdb519ea820f726d61c
Hector Alarcon Guerra <serproemcamseguridadprivada@gmail.com>
2024-12-25 19:51:12 -0500

    imagenes-parallax

--- app/(drawer)/(tabs)/home/index.tsx
+++ app/(drawer)/(tabs)/home/index.tsx
@@ -1,17 +1,30 @@
+import { View, Text, ActivityIndicator, SafeAreaView } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 import { useMovies } from '@/presentation/hooks/useMovies'
-import { View, Text } from 'react-native'
+import MainSlideshow from '@/presentation/components/movies/MainSlideshow';
+
 
 const HomeScreen = () => {
 
+  const safeArea = useSafeAreaInsets();
   const { nowPlayingQuery } = useMovies();
 
 
+  if (nowPlayingQuery.isLoading) {
+    return (
+      <View className='justify-center items-center flex-1'>
+        <ActivityIndicator color='purple' size={40} />
+      </View>
+    );
+  }
+
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Text>
-        {JSON.stringify(nowPlayingQuery.data)}
-      </Text>
+    <View className='mt-2' style={{ paddingTop: safeArea.top }}>
+      <Text className='text-3xl font-bold px-4 mb-2'>Parqueaderos</Text>
+      {/* Carusel de imágenes */}
+      <MainSlideshow movies={nowPlayingQuery.data ?? []} />
     </View>
   )
 }
---
+++ presentation/components/movies/MainSlideshow.tsx
@@ -0,0 +1,42 @@
+import { useRef } from 'react';
+import { View, Text, useWindowDimensions } from 'react-native'
+import Carousel, { ICarouselInstance } from 'react-native-reanimated-carousel';
+import { Movie } from '@/infrastructure/interfaces/movie.interface';
+import MoviePoster from './MoviePoster';
+
+interface Props {
+    movies: Movie[];
+}
+
+const MainSlideshow = ({ movies }: Props) => {
+
+    const ref = useRef<ICarouselInstance>(null);
+    const width = useWindowDimensions().width;
+
+    return (
+        <View className='h-[250] w-full'>
+            <Carousel
+                ref={ref}
+                data={movies}
+                renderItem={({ item }) => <MoviePoster id={item.id} poster={item.poster} />}
+                width={200}
+                height={350}
+                style={{
+                    width: width,
+                    height: 350,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+                mode='parallax'
+                modeConfig={{
+                    parallaxScrollingScale: 0.9,
+                    parallaxScrollingOffset: 50,
+                }}
+                defaultIndex={1}
+
+            />
+        </View>
+    )
+}
+
+export default MainSlideshow
