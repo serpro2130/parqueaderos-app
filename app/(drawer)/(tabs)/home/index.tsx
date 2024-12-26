56a18282fd6ef44f5c2ada15a6ad85faa90729c3
Hector Alarcon Guerra <serproemcamseguridadprivada@gmail.com>
2024-12-25 20:42:44 -0500

    Consultas de varios endpoints

--- app/(drawer)/(tabs)/home/index.tsx
+++ app/(drawer)/(tabs)/home/index.tsx
@@ -1,14 +1,15 @@
-import { View, Text, ActivityIndicator, SafeAreaView } from 'react-native'
+import { View, Text, ActivityIndicator, SafeAreaView, ScrollView } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { useMovies } from '@/presentation/hooks/useMovies'
 import MainSlideshow from '@/presentation/components/movies/MainSlideshow';
+import MovieHorizontalList from '@/presentation/components/movies/MovieHorizontalList';
 
 
 const HomeScreen = () => {
 
   const safeArea = useSafeAreaInsets();
-  const { nowPlayingQuery } = useMovies();
+  const { nowPlayingQuery, popularQuery, upcomingQuery, topRatedQuery } = useMovies();
 
 
   if (nowPlayingQuery.isLoading) {
@@ -21,11 +22,28 @@ const HomeScreen = () => {
 
 
   return (
-    <View className='mt-2' style={{ paddingTop: safeArea.top }}>
-      <Text className='text-3xl font-bold px-4 mb-2'>Parqueaderos</Text>
-      {/* Carusel de imágenes */}
-      <MainSlideshow movies={nowPlayingQuery.data ?? []} />
-    </View>
+    <ScrollView>
+      <View className='mt-2 pb-10' style={{ paddingTop: safeArea.top }}>
+        <Text className='text-3xl font-bold px-4 mb-2'>Parqueaderos</Text>
+        {/* Carusel de imágenes */}
+        <MainSlideshow movies={nowPlayingQuery.data ?? []} />
+        {/* Popular */}
+        <MovieHorizontalList
+          title="Populares" movies={popularQuery.data ?? []}
+          className='mb-5'
+        />
+        {/* Top Rated */}
+        <MovieHorizontalList
+          title="Mejor Calificadas" movies={topRatedQuery.data ?? []}
+          className='mb-5'
+        />
+        {/* Próximamnete */}
+        <MovieHorizontalList
+          title="Próximamente" movies={upcomingQuery.data ?? []}
+          className='mb-5'
+        />
+      </View>
+    </ScrollView>
   )
 }
 
--- presentation/hooks/useMovies.tsx
+++ presentation/hooks/useMovies.tsx
@@ -1,7 +1,7 @@
-// import { popularMoviesAction } from '@/core/ actions/movies/popular.action';
-// import { topRatedMoviesAction } from '@/core/ actions/movies/top-rated.action';
-// import { upcomingMoviesAction } from '@/core/ actions/movies/upcoming.action';
 import { nowPlayingAction } from '@/core/actions/movies/now-playing.action';
+import { popularMoviesAction } from '@/core/actions/movies/popular.action';
+import { topRatedMoviesAction } from '@/core/actions/movies/top-rated.action';
+import { upcomingMoviesAction } from '@/core/actions/movies/upcoming.action';
 import { useQuery } from '@tanstack/react-query';
 
 export const useMovies = () => {
@@ -12,30 +12,28 @@ export const useMovies = () => {
         staleTime: 1000 * 60 * 60 * 24, // 24horas
     })
 
+    const popularQuery = useQuery({
+        queryKey: ['movies', 'popular'],
+        queryFn: popularMoviesAction,
+        staleTime: 1000 * 60 * 60 * 24, // 24horas
+    });
 
+    const topRatedQuery = useQuery({
+        queryKey: ['movies', 'top-rated'],
+        queryFn: topRatedMoviesAction,
+        staleTime: 1000 * 60 * 60 * 24, // 24horas
+    });
 
-    //   const popularQuery = useQuery({
-    //     queryKey: ['movies', 'popular'],
-    //     queryFn: popularMoviesAction,
-    //     staleTime: 1000 * 60 * 60 * 24, // 24horas
-    //   });
-
-    //   const topRatedQuery = useQuery({
-    //     queryKey: ['movies', 'top-rated'],
-    //     queryFn: topRatedMoviesAction,
-    //     staleTime: 1000 * 60 * 60 * 24, // 24horas
-    //   });
-
-    //   const upcomingQuery = useQuery({
-    //     queryKey: ['movies', 'upcoming'],
-    //     queryFn: upcomingMoviesAction,
-    //     staleTime: 1000 * 60 * 60 * 24, // 24horas
-    //   });
+    const upcomingQuery = useQuery({
+        queryKey: ['movies', 'upcoming'],
+        queryFn: upcomingMoviesAction,
+        staleTime: 1000 * 60 * 60 * 24, // 24horas
+    });
 
     return {
         nowPlayingQuery,
-        // popularQuery,
-        // topRatedQuery,
-        // upcomingQuery,
+        popularQuery,
+        topRatedQuery,
+        upcomingQuery,
     };
 };
